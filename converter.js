4737deedc31122e2f515b0d6fa585c5c385439c6
darnaud <twists@live.fr>
2015-03-09 22:16:53 -0500

    Testing

--- converter.js
+++ converter.js
@@ -3,24 +3,32 @@
 
 
 
-
+do {
 var choice = parseInt(prompt('Entrez un nombre entre 0 et 999: '));
+} while ( !choice)
+
 
 var unit = (choice % 10); 
 
-var decimal = (choice % 100) - unit; 
+var decimal = (choice % 100) - unit;
 
-var hundred = choice - (unit + decimal);
+var dcmUnit = (choice % 100); 
+
+var hundred = (choice - (unit + decimal))/100 ;
 
 var numString = '';
+
+var isHundred = true ;
+
+var isDec = false ; 
 
 function scanDigit(arg){
 
 	switch (arg){
 
 		case 0:
-		
-			numString += 'Zero';
+
+			(!isHundred && !isDec)? numString += 'Zero' : isDecimal = true ;
 			break;
 			
 		case 1:
@@ -28,48 +36,153 @@ function scanDigit(arg){
 			break;
 			
 		case 2:
-			numString += 'Deux';
+			numString += 'Deux-';
 			break;
 			
 		case 3:
-			numString += 'Trois';
+			numString += 'Trois-';
 			break;
 			
 		case 4:
-			numString += 'Quatre';
+			numString += 'Quatre-';
 			break;
 			
 		case 5:
-			numString += 'Cinq';
+			numString += 'Cinq-';
 			break;
 		
 		case 6:
-			numString += 'Six';
+			numString += 'Six-';
 			break;
 		
 		case 7:
-			numString += 'Sept';
+			numString += 'Sept-';
 			break;
 		
 		case 8:
-			numString += 'Huit';
+			numString += 'Huit-';
 			break;
 		
 		case 9:
-			numString += 'Neuf';
+			numString += 'Neuf-';
+			break;
+
+		case 10:
+			numString += 'Dix-';
+			break;
+
+		case 11:
+			numString += 'Onze';
+			break;
+
+		case 12:
+			numString += 'Douze';
+			break;
+
+		case 13:
+			numString += 'Treize';
+			break;
+
+		case 14:
+			numString += 'Quatorze';
+			break;
+
+		case 15:
+			numString += 'Quinze';
+			break;
+
+		case 16:
+			numString += 'Seize';
+			break;
+
+		case 20:
+			numString += 'Vingt-';
+			break;
+
+		case 30:
+			numString += 'Trente-';
+			break;
+
+		case 40:
+			numString += 'Quarante-';
+			break;
+
+		case 50:
+			numString += 'Cinquante-';
+			break;
+
+		case 60:
+			numString += 'Soixante-';
+			break;
+
+		case 70:
+			numString += 'Soixante-Dix-';
+			break;
+
+		case 80:
+			numString += 'Quatre-Vingt-';
+			break;
+
+		case 90:
+			numString += 'Quatre-Vingt-Dix-';
 			break;
 		
 		default:
-			alert('Ce nombre ne fait pas partie de l\'intervalle specifié!');
+			scanDigit(decimal);
+			scanDigit(unit);
+
 			
 	}
 }
 
-scanDigit(choice);
+
+
+//alert(hundred + ' ' + decimal + ' ' + unit);
+
+//Identify hundred
+
+isHundred = true ;
+
+scanDigit(hundred);
+
+if (hundred == 1){
+
+	numString = 'Cent-';
+}
+else if (hundred != 0 && hundred !=1 ){
+
+	numString += 'Cent-';
+}
+
+isHundred = false;
+
+//Identify decimal
+
+
+scanDigit(dcmUnit);
 
 alert(choice + ' : ' + numString);
 
-alert(hundred + ' ' + decimal + ' ' + unit);
 
-//Identify hundred
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
